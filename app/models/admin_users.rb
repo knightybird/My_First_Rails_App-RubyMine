9efcdbecb5660dafda0f615da422e4cb32f9f41c
knightybird <nicholasoentoro@gmail.com>
2020-03-27 22:08:40 -0700

    UPDATE added database and models by generating migrations currently learning models and Active Record
    +database
    +migrations - models
    +record

---
+++ app/models/admin_users.rb
@@ -0,0 +1,7 @@
+class AdminUsers < ApplicationRecord
+
+  #cant find table name users, fix: change to admin_users
+  # self.table_name = "admin_users"
+
+  #alternate solution: change name of physical file to "admin_users"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,55 @@
+#Route Types
+# simple route, default route, root route, resourceful routes
+#
+# simple (match) route
+#
+#   //example
+#
+#     get demo/index
+#     match "demo/index", :to => "demo#index", :via => :
+#
+# default route
+#    //structure:
+#     :controller/:action/:id
+#    //
+#     get ':controller(/:action(/:id))'
+#     match ':controller(/:action(/:id))', :via => :get
+#
+#   //example
+#
+#     GET /students/edit/52
+#
+#   //is represented as...
+#     StudentsController, edit action, id = 52
+#
+#
+# root route
+#   //structure
+#     match "/", :to => "demo#index", :via => :get
+#
+#   //example
+#
+#     root "demo#index"
+#
+# resourceful routes
+#
 Rails.application.routes.draw do
-  get 'demo/index'
+  get 'subjects/index'
+  get 'subjects/show'
+  get 'subjects/new'
+  get 'subjects/edit'
+  get 'subjects/delete'
+#Simple Route example
+
+  # get 'demo/index'
+
+#Default Route example
+# may go away in future versions of Rails
+
+  # get ':controller(/:action(/:id))'
+
+#Root Route example
+  root "demo#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
